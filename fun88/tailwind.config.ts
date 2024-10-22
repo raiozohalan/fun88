e7bd25d966742c3e521109561e043253bb631628
Joelhy Galan <raizohgalan@gmail.com>
2024-10-22 23:21:40 +0800

    feat: Add primary, secondary, and tertiary colors to tailwind.config.ts

--- fun88/tailwind.config.ts
+++ fun88/tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#00A6FF",
+        secondary: "#FFD600",
+        tertiary: "#888888",
       },
     },
   },

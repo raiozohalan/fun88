9a3428d8cac41a753df1b61fc682ac4419bb8b7a
Joelhy Galan <raizohgalan@gmail.com>
2024-10-23 11:43:11 +0800

    refactor: make svg assets to react component

---
+++ src/components/assets/footer/index.tsx
@@ -0,0 +1,8 @@
+import Cashier from "./Cashier";
+import CasinoLive from "./CasinoLive";
+import Favorite from "./Favorite";
+import Invite from "./Invite";
+import Sports from "./Sports";
+
+// Export all assets
+export { Cashier, CasinoLive, Favorite, Invite, Sports };
---
+++ src/components/assets/game-category/index.tsx
@@ -0,0 +1,23 @@
+import Bingo from "./Bingo";
+import Jackpot from "./Jackpot";
+import Live from "./Live";
+import New from "./New";
+import Others from "./Others";
+import Search from "./Search";
+import Slots from "./Slots";
+import Start from "./Start";
+import Table from "./Table";
+// Add more imports as needed
+
+export {
+  Bingo,
+  Jackpot,
+  Live,
+  New,
+  Others,
+  Search,
+  Slots,
+  Start,
+  Table,
+  // Add more exports as needed
+};

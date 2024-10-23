2ee932bf167e8972a3ad04252aad322ad069e92b
Joelhy Galan <raizohgalan@gmail.com>
2024-10-23 20:41:20 +0800

    refactor: Update game provider list data structure

--- src/components/category-list/GameProviderMenu/game-provider-list.ts
+++ src/components/category-list/GameProviderMenu/game-provider-list.ts
@@ -1,20 +1,26 @@
-export default [
-  "/images/game-providers/EM.png",
-  "/images/game-providers/EVO.png",
-  "/images/game-providers/EXPANSE.png",
-  "/images/game-providers/EZG.png",
-  "/images/game-providers/GAMEART.png",
-  "/images/game-providers/HAB.png",
-  "/images/game-providers/HACKSAW.png",
-  "/images/game-providers/INBET.png",
-  "/images/game-providers/MPLAY.png",
-  "/images/game-providers/NETENT.png",
-  "/images/game-providers/PGSOFT.png",
-  "/images/game-providers/PNG.png",
-  "/images/game-providers/PP.png",
-  "/images/game-providers/PRAGMATICPLAY.png",
-  "/images/game-providers/PS.png",
-  "/images/game-providers/PT.png",
-  "/images/game-providers/REDTIGER.png",
-  "/images/game-providers/RELAX.png",
+import { GameProvider } from "@/context/interface";
+
+const ROOT_PATH = "/images/game-providers/";
+
+const DUMMY_GAME_PROVIDERS: GameProvider[] = [
+  { name: "EM", id: "1", logo: `${ROOT_PATH}EM.png` },
+  { name: "EVO", id: "2", logo: `${ROOT_PATH}EVO.png` },
+  { name: "EXPANSE", id: "3", logo: `${ROOT_PATH}EXPANSE.png` },
+  { name: "EZG", id: "4", logo: `${ROOT_PATH}EZG.png` },
+  { name: "GAME ART", id: "5", logo: `${ROOT_PATH}GAMEART.png` },
+  { name: "HAB", id: "6", logo: `${ROOT_PATH}HAB.png` },
+  { name: "HACKSAW", id: "7", logo: `${ROOT_PATH}HACKSAW.png` },
+  { name: "INBET", id: "8", logo: `${ROOT_PATH}INBET.png` },
+  { name: "MPLAY", id: "9", logo: `${ROOT_PATH}MPLAY.png` },
+  { name: "NETENT", id: "10", logo: `${ROOT_PATH}NETENT.png` },
+  { name: "PGSOFT", id: "11", logo: `${ROOT_PATH}PGSOFT.png` },
+  { name: "PNG", id: "12", logo: `${ROOT_PATH}PNG.png` },
+  { name: "PP", id: "13", logo: `${ROOT_PATH}PP.png` },
+  { name: "PRAGMATIC PLAY", id: "14", logo: `${ROOT_PATH}PRAGMATICPLAY.png` },
+  { name: "PS", id: "15", logo: `${ROOT_PATH}PS.png` },
+  { name: "PT", id: "16", logo: `${ROOT_PATH}PT.png` },
+  { name: "RED TIGER", id: "17", logo: `${ROOT_PATH}REDTIGER.png` },
+  { name: "RELAX", id: "18", logo: `${ROOT_PATH}RELAX.png` },
 ];
+
+export default DUMMY_GAME_PROVIDERS;

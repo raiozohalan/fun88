0b6ac58af0290d4550ba3b292be82878b65216df
Joelhy Galan <raizohgalan@gmail.com>
2024-10-23 13:54:04 +0800

    feat: Add SearchGames component to CategoryList

--- src/components/category-list/CategoryList.tsx
+++ src/components/category-list/CategoryList.tsx
@@ -5,6 +5,7 @@ import CATEGORY_LIST from "./categories";
 import classNames from "@/utils/classNames";
 import CategoryItem from "./CategoryItem";
 import { Search } from "../assets/game-category";
+import SearchGames from "./SearchGames";
 
 const CategoryList = () => {
   const [activeTab, setActiveTab] = useState<string>("start");
@@ -15,35 +16,39 @@ const CategoryList = () => {
     },
     [activeTab]
   );
+
   return (
-    <div
-      className={classNames(
-        "max-w-full h-fit bg-white pl-2",
-        "flex-none flex items-center justify-evenly"
-      )}
-    >
-      <CategoryItem
-        title="Search"
-        Icon={Search}
-        isActive={isActiveTab("Search")}
-        onClick={setActiveTab}
-      />
-      <div className="block w-[1px] h-[30px] bg-[#88888880]" />
-      <div className="flex items-center gap-1  overflow-x-auto overflow-y-hidden hide-scrollbar snap-proximity snap-x">
-        {CATEGORY_LIST.map(({ title, Icon }, index) => {
-          const isActive = isActiveTab(title);
-          return (
-            <CategoryItem
-              key={index}
-              title={title}
-              Icon={Icon}
-              isActive={isActive}
-              onClick={setActiveTab}
-            />
-          );
-        })}
+    <>
+      <div
+        className={classNames(
+          "max-w-full h-fit bg-white pl-2",
+          "flex-none flex items-center justify-evenly"
+        )}
+      >
+        <CategoryItem
+          title="Search"
+          Icon={Search}
+          isActive={isActiveTab("Search")}
+          onClick={setActiveTab}
+        />
+        <div className="block w-[1px] h-[30px] bg-[#88888880]" />
+        <div className="flex items-center gap-1  overflow-x-auto overflow-y-hidden hide-scrollbar snap-proximity snap-x">
+          {CATEGORY_LIST.map(({ title, Icon }, index) => {
+            const isActive = isActiveTab(title);
+            return (
+              <CategoryItem
+                key={index}
+                title={title}
+                Icon={Icon}
+                isActive={isActive}
+                onClick={setActiveTab}
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
+      {isActiveTab("Search") && <SearchGames />}
+    </>
   );
 };
 
---
+++ src/components/category-list/SearchGames.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Search } from "../assets/game-category";
+import classNames from "@/utils/classNames";
+import Button from "../base/button/Button";
+import SearchMenu from "../assets/SearchMenu";
+
+const SearchGames = () => {
+  return (
+    <div className="flex gap-3 w-full px-4 py-2">
+      <div
+        className={classNames(
+          "flex-1 flex gap-1 pl-3 h-fit",
+          "ring-1 ring-primary rounded-md items-center"
+        )}
+      >
+        <Search className="flex-none size-5 text-tertiary" />
+        <input
+          type="search"
+          name="search"
+          placeholder="Search games"
+          className={classNames(
+            "w-full h-full flex-1 px-2 py-2 rounded-l-none rounded-r-md",
+            "focus:outline-none focus:ring-0 focus:ring-transparent focus:border-primary"
+          )}
+        />
+      </div>
+      <Button size="xs" className="flex-none ring-1 ring-tertiary rounded-md">
+        <SearchMenu className="size-5 text-tertiary" />
+      </Button>
+    </div>
+  );
+};
+
+export default SearchGames;

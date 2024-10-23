ddec50e2eeb1648e4ef271b475aa08ce97b1b9bc
Joelhy Galan <raizohgalan@gmail.com>
2024-10-23 10:01:43 +0800

    refactor: update assets source for header icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,24 +14,24 @@ const Header = () => {
     >
       <div className="flex-1 flex items-center justify-start gap-1">
         <Button size="xs">
-          <SvgWrapper src="/assets/menu.svg" className="h-4 w-4" />
+          <SvgWrapper src="/assets/header/menu.svg" className="h-4 w-4" />
         </Button>
-        <SvgWrapper src="/assets/fun88.svg" className="h-5 w-auto" />
+        <SvgWrapper src="/assets/header/fun88.svg" className="h-5 w-auto" />
       </div>
       <div className="flex items-center justify-end">
         <div className="flex gap-2 items-center">
           <SvgWrapper
-            src="/assets/wallet.svg"
+            src="/assets/header/wallet.svg"
             className="h-[18px] w-[18px] -mt-1"
           />
           <b className="text-base text-primary">
             $
             {String(DUMMY_WALLET_BALANCE).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
           </b>
-          <SvgWrapper src="/assets/divider.svg" className="h-[34px] w-auto" />
+          <SvgWrapper src="/assets/header/divider.svg" className="h-[34px] w-auto" />
         </div>
         <Button size="xs">
-          <SvgWrapper src="/assets/user.svg" className="h-6 w-6" />
+          <SvgWrapper src="/assets/header/user.svg" className="h-6 w-6" />
         </Button>
       </div>
     </div>

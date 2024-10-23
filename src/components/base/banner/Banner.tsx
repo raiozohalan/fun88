a193c53bd99fa5dade84774c34a3d9c7594ea854
Joelhy Galan <raizohgalan@gmail.com>
2024-10-23 13:24:12 +0800

    Refactor: Update padding in Banner component

--- src/components/base/banner/Banner.tsx
+++ src/components/base/banner/Banner.tsx
@@ -5,7 +5,7 @@ import SvgWrapper from "../wrapper/SvgWrapper";
 
 const Banner = () => {
   return (
-    <div className="flex flex-col gap-2 w-full p-3">
+    <div className="flex flex-col gap-2 w-full px-3 pt-3 pb-2">
       <Main />
       <p className="flex items-center gap-2 text-sm text-primary">
         <SvgWrapper src="/assets/bell.svg" className="w-4 h-4" />
